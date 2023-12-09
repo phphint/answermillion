e92dc8d1d3b3f37d1e6c0ba022612f715cafad8d
phphint <124293060+phphint@users.noreply.github.com>
2023-12-09 04:42:53 -0500

    building app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,19 +14,19 @@ root.render(
     <HelmetProvider>
     <Helmet>
         {/* Favicon */}
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         {/* Apple Touch Icon */}
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/apple-touch-icon.png" sizes="180x180" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
         {/* Android and other devices */}
-        <link rel="icon" href="%PUBLIC_URL%/icons/icon-192x192.png" sizes="192x192" type="image/png" />
-        <link rel="icon" href="%PUBLIC_URL%/icons/icon-256x256.png" sizes="256x256" type="image/png" />
-        <link rel="icon" href="%PUBLIC_URL%/icons/icon-384x384.png" sizes="384x384" type="image/png" />
-        <link rel="icon" href="%PUBLIC_URL%/icons/icon-512x512.png" sizes="512x512" type="image/png" />
+        <link rel="icon" href="/icons/icon-192x192.png" sizes="192x192" type="image/png" />
+        <link rel="icon" href="/icons/icon-256x256.png" sizes="256x256" type="image/png" />
+        <link rel="icon" href="/icons/icon-384x384.png" sizes="384x384" type="image/png" />
+        <link rel="icon" href="/icons/icon-512x512.png" sizes="512x512" type="image/png" />
         {/* Microsoft Tiles */}
-        <meta name="msapplication-square70x70logo" content="%PUBLIC_URL%/icons/smalltile.png" />
-        <meta name="msapplication-square150x150logo" content="%PUBLIC_URL%/icons/mediumtile.png" />
-        <meta name="msapplication-wide310x150logo" content="%PUBLIC_URL%/icons/widetile.png" />
-        <meta name="msapplication-square310x310logo" content="%PUBLIC_URL%/icons/largetile.png" />
+        <meta name="msapplication-square70x70logo" content="/icons/smalltile.png" />
+        <meta name="msapplication-square150x150logo" content="/icons/mediumtile.png" />
+        <meta name="msapplication-wide310x150logo" content="/icons/widetile.png" />
+        <meta name="msapplication-square310x310logo" content="/icons/largetile.png" />
       </Helmet> 
     <App />
     </HelmetProvider>
